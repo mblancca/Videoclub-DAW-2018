9bddbb6e9ca6784f180fa2bc4aabb9adae0bf49a
marianoblancocantero <mariano.blanco.cantero@alumnos.upm.es>
2018-05-18 12:26:34 +0200

    a

--- DAWdjango/mysite/myapp/urls.py
+++ DAWdjango/mysite/myapp/urls.py
@@ -4,8 +4,5 @@ from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-	path('',views.index, name='index'),
-	path('other/test',views.other, name='other'),
-	path('form',views.form,name='form'),
-	path('process_form',views.process_form,name='process_form')
+	path('', views.prueba, name='prueba')
 ]
--- DAWdjango/mysite/myapp/views.py
+++ DAWdjango/mysite/myapp/views.py
@@ -6,37 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-visit_count = 0
 
-def index(request):
-	global visit_count
-	visit_count += 1
-	template=loader.get_template("myapp/index.html")
-	context = {
-	}
-	return HttpResponse(template.render(context, request))
-	
-def other(request):
-	template=loader.get_template("myapp/other.html")
-	context = {
-		'item_list' : [
-			'Red',
-			'Green',
-			'Blue'
-		]
-	}
-	return HttpResponse(template.render(context, request))
-
-def form(request):
-	context = {}
-	return render(request, "myapp/form.html", context)
-
-texts = []
-
-def process_form(request):
-	context = {}
-	if request.method == 'POST':
-		input_text = request.POST['input_text']
-		texts.append(input_text)
-		context['texts'] = texts
-	return render(request, "myapp/process_form.html", context)
+def prueba(request):
+    template = loader.get_template("myapp/Index.html")
+    return HttpResponse(template.render())
