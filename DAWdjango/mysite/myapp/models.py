282c30b3be5d14a1a7ae9ef706adac63a9489012
marianoblancocantero <mariano.blanco.cantero@alumnos.upm.es>
2018-05-19 13:49:28 +0200

    regsitro inicio logout etc

--- DAWdjango/mysite/myapp/models.py
+++ DAWdjango/mysite/myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -16,3 +19,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255, blank=True)
+    web = models.URLField(blank=True)
+
+    # Python 3
+    def __str__(self):
+        return self.usuario.username
+
+
+@receiver(post_save, sender=User)
+def crear_usuario_perfil(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(usuario=instance)
+
+
+@receiver(post_save, sender=User)
+def guardar_usuario_perfil(sender, instance, **kwargs):
+    instance.perfil.save()
--- DAWdjango/mysite/myapp/urls.py
+++ DAWdjango/mysite/myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from . import views
-from django.conf.urls import include
+from django.conf.urls import include, url
+from myapp.views import SignUpView, BienvenidaView, SignInView, SignOutView
 
 urlpatterns = [
-	path('', views.login, name='login'),
-	path('index/', views.index, name='index')	
+	#path('', views.acceso, name='acceso'),
+	path('index/', views.index, name='index'),
+	url(r'^$', BienvenidaView.as_view(), name='bienvenida'),
+    url(r'^registrate/$', SignUpView.as_view(), name='sign_up'),
+	url(r'^incia-sesion/$', SignInView.as_view(), name='sign_in'),
+	url(r'^cerrar-sesion/$', SignOutView.as_view(), name='sign_out'),
 ]
