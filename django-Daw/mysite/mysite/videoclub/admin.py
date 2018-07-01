b3330aa69499cc48deb1d28639defa3d319e5fee
Alvaro <alvaroto14@hotmail.com>
2018-07-01 21:22:25 +0200

    carpeta borrada

--- django-Daw/mysite/mysite/videoclub/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Persona, Genero, Pelicula
-
-admin.site.register(Pelicula)
-admin.site.register(Persona)
-admin.site.register(Genero)
