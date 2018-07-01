289a4267befd3874dff44000f5d030488212d881
marianoblancocantero <mariano.blanco.cantero@alumnos.upm.es>
2018-07-01 21:24:14 +0200

    backup

---
+++ django-Daw/django-Daw/mysite/mysite/videoclub/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.urls import reverse
+#####################################################
+
+class Genero(models.Model):
+    name = models.CharField(max_length=200, help_text="Introduzca un género de película")    
+    def __str__(self):        
+        return self.name
+
+#####################################################
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100, default="")
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+    esDirector = models.CharField(max_length=2, default="no",help_text="si/no , en minusculas todo")
+    urlFoto = models.CharField(max_length=200, blank=False, default="https://www.yueimg.com/en/images/common/avatar.b6a87.png")
+       
+    def get_absolute_url(self):        
+        return reverse('persona-detalle', args=[str(self.id)])
+    
+    def get_foto(self):
+        return self.urlFoto
+
+    def __str__(self):
+        return self.nombre       
+
+#####################################################
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=200, blank=False, default="")
+    url = models.CharField(max_length=200, blank=False, default="")
+    descripcion = models.TextField(max_length=1000, default="", help_text="Introduce una descripcion para la película")
+    año = models.DateField(null=True, blank=True)
+    models.DateField(null=True, blank=True)
+    director = models.ForeignKey(Persona, related_name='director',on_delete=models.SET_NULL, null=True)
+    reparto = models.ManyToManyField(Persona)
+    urlPortada = models.CharField(max_length=200, default="")
+    genero = models.ManyToManyField(Genero, help_text="Selecciona un género para esta pelicula")
+    valoracion = models.PositiveIntegerField(help_text="Valor de 0-100")
+
+    def __str__(self):
+        return self.titulo    
+    
+    def get_portada(self):
+        return self.urlPortada
+
+    def get_url(self):
+        return self.url
+
+    def get_genero(self):
+        return self.genero
+
+    def get_absolute_url(self):
+        return reverse('pelicula-detalle', args=[str(self.id)])
+
+#####################################################
---
+++ django-Daw/django-Daw/mysite/mysite/videoclub/views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import render
+from .models import Pelicula, Persona, Genero
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import operator
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+@login_required
+def index(request):
+    listaPeliculas1 = Pelicula.objects.all().filter(
+        genero__name__icontains='Drama').order_by('-id')[:4]  # Coge las 2 primeras
+    listaPeliculas2 = Pelicula.objects.all().filter(
+        genero__name__icontains='Acción').order_by('id')[:4]  # Coge las 2 ultimas
+
+    listaPeliculas3 = Pelicula.objects.all().filter(
+        genero__name__icontains='Ciencia Ficción').order_by('-id')[:4]  # Coge las 2 primeras
+    listaPeliculas4 = Pelicula.objects.all().filter(
+        genero__name__icontains='Comedia').order_by('id')[:4]  # Coge las 2 ultimas
+
+    numero_visitas = request.session.get('numero_visitas', 0)
+    request.session['numero_visitas'] = numero_visitas+1
+
+    return render(
+        request,
+        'index.html',
+        context={'listaPeliculas1': listaPeliculas1,
+                 'listaPeliculas2': listaPeliculas2,
+                 'listaPeliculas3': listaPeliculas3,
+                 'listaPeliculas4': listaPeliculas4}
+    )
+
+
+class PeliculaListView(LoginRequiredMixin, generic.ListView):
+    model = Pelicula
+    paginate_by = 4
+
+    def get_queryset(self):
+        listaDePeliculas = self.model.objects.all()
+        return listaDePeliculas
+
+
+class PeliculaDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Pelicula
+
+
+def pelicula_detail_view(request, pk):
+    try:
+        pelicula_id = Pelicula.objects.get(pk=pk)
+    except Pelicula.DoesNotExist:
+        raise Http404("Pelicula does not exist")
+
+    return render(
+        request,
+        'videoclub/pelicula_detail.html',
+        context={'pelicula': pelicula_id, }
+    )
+
+# Reparto de Directores
+
+
+class DirectorListView(LoginRequiredMixin, generic.ListView):
+    model = Persona
+    paginate_by = 10
+    template_name = 'videoclub/director_list.html'
+
+    def get_queryset(self):
+        listaDeDirectores = self.model.objects.filter(
+            esDirector__icontains='si')
+        return listaDeDirectores
+
+# Reparto de actores
+
+
+class ActorListView(LoginRequiredMixin, generic.ListView):
+    model = Persona
+    paginate_by = 10
+    template_name = 'videoclub/actor_list.html'
+
+    def get_queryset(self):
+        listaDeActores = self.model.objects.filter(esDirector__icontains='no')
+        return listaDeActores
+
+
+class PersonaDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Persona
+    template_name = 'videoclub/person_detail.html'
+
+# BuscarPeliculas
+
+
+def search_form(request):
+    return render(request, 'videoclub/search_form.html')
+
+
+def search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        contadorPeliculas = Pelicula.objects.filter(
+            titulo__icontains=q).count()
+        contadorPersonas = Persona.objects.filter(nombre__icontains=q).count()
+        contadorGeneros = Genero.objects.filter(name__icontains=q).count()
+
+        if contadorPeliculas != 0:
+            peliculas = Pelicula.objects.filter(titulo__icontains=q)
+            return render(request, 'videoclub/search_results.html',
+                          {'peliculas': peliculas, 'query': q})
+        elif contadorPersonas != 0:
+            personas = Persona.objects.filter(nombre__icontains=q)
+            return render(request, 'videoclub/search_results.html',
+                          {'personas': personas, 'query': q})        
+        else:
+            return render(request, 'videoclub/search_results.html',
+                          {'query': q})
+
+    else:
+        return render(request, 'videoclub/search_error.html')
+
+# BAD!
+def bad_search(request):
+    # The following line will raise KeyError if 'q' hasn't been submitted!
+    message = 'You searched for: %r' % request.GET['q']
+    return HttpResponse(message)
